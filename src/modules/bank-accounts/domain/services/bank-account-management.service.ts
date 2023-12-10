68f223c4fdb6b4aeffa46bf3ae242571ff94b809
Erick Sousa <erick.sousa@nave.rs>
2023-12-10 04:34:03 -0300

    feat: add fuction on bank account service to call prisma fuction to get all bank accounts on db

--- src/modules/bank-accounts/domain/services/bank-account-management.service.ts
+++ src/modules/bank-accounts/domain/services/bank-account-management.service.ts
@@ -6,6 +6,7 @@ import { BankAccountRepository } from '@src/modules/bank-accounts/domain/interfa
 
 import { CreateBankAccountInputDto } from '@src/modules/bank-accounts/http/dtos/bank-account/create-bank-account-dto'
 import { UpdateBankAccountInputDto } from '@src/modules/bank-accounts/http/dtos/bank-account/update-bank-account-dto'
+import { GetAllBankAccountsOutputDto } from '@src/modules/bank-accounts/http/dtos/bank-account/get-all-bank-accounts-dto'
 
 @Injectable()
 export class BankAccountManagementService {
@@ -36,5 +37,12 @@ export class BankAccountManagementService {
 
   async findOneBankAccount(bankAccountId: string): Promise<BankAccountEntity> {
     return this.bankAccountRepository.findOne(bankAccountId)
+  }
+
+  async findAllBankAccounts(
+    page?: number,
+    pageSize?: number
+  ): Promise<GetAllBankAccountsOutputDto> {
+    return this.bankAccountRepository.findAll(page, pageSize)
   }
 }
