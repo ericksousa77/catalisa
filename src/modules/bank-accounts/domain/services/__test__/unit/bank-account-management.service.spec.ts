4086f0c30778af0f06fa7497985a1b9ff6bbe5fa
Erick Sousa <erick.sousa@nave.rs>
2023-12-10 00:35:35 -0300

    feat: add method on bank accounts service to call repository function

--- src/modules/bank-accounts/domain/services/__test__/unit/bank-account-management.service.spec.ts
+++ src/modules/bank-accounts/domain/services/__test__/unit/bank-account-management.service.spec.ts
@@ -1,5 +1,73 @@
-describe('Testes de soma', () => {
-  test('1 + 1 é igual a 2', () => {
-    expect(1 + 1).toBe(2)
+import { Test, TestingModule } from '@nestjs/testing'
+
+import { BankAccountEntity } from '@src/modules/bank-accounts/domain/entities/bank-account.entity'
+
+import { BankAccountRepository } from '@src/modules/bank-accounts/domain/interfaces/bank-account.repository.interface'
+
+import { BankAccountManagementService } from '@src/modules/bank-accounts/domain/services/bank-account-management.service'
+
+import { CreateBankAccountInputDto } from '@src/modules/bank-accounts/http/dtos/bank-account/create-bank-account-dto'
+
+describe('BankAccountManagementService', () => {
+  let service: BankAccountManagementService
+  let bankAccountRepository: BankAccountRepository
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        BankAccountManagementService,
+        {
+          provide: BankAccountRepository,
+          useValue: {
+            save: jest.fn()
+          }
+        }
+      ]
+    }).compile()
+
+    service = module.get<BankAccountManagementService>(
+      BankAccountManagementService
+    )
+
+    bankAccountRepository = module.get<BankAccountRepository>(
+      BankAccountRepository
+    )
+  })
+
+  describe('create', () => {
+    it('should call bank account repository function to save bank account and return bank account created', async () => {
+      const bankAccountEntity = BankAccountEntity.create({
+        agency: 'Mocked Agency',
+        type: 'POUPANCA',
+        balance: 0
+      })
+
+      const bankAccountToCreateProps: CreateBankAccountInputDto = {
+        agency: bankAccountEntity.agency,
+        type: bankAccountEntity.type
+      }
+
+      jest.spyOn(bankAccountRepository, 'save').mockResolvedValue({
+        ...bankAccountEntity,
+        accountNumber: 2,
+        isActive: true
+      })
+
+      const result = await service.create(bankAccountToCreateProps)
+
+      expect(bankAccountRepository.save).toBeCalledTimes(1)
+      expect(bankAccountRepository.save).toBeCalledWith(
+        expect.objectContaining({
+          agency: bankAccountEntity.agency,
+          type: bankAccountEntity.type
+        })
+      )
+
+      expect(result).toEqual({
+        ...bankAccountEntity,
+        accountNumber: 2,
+        isActive: true
+      })
+    })
   })
 })
