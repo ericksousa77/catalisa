cc0bb4a24ff5b0c504aa4a44cfb0e8fb8721636e
Erick Sousa <erick.sousa@nave.rs>
2023-12-10 00:34:15 -0300

    feat: add prisma method to save bank account entity on database

--- src/modules/bank-accounts/repositories/__test__/unit/bank-account.prisma.repository.spec.ts
+++ src/modules/bank-accounts/repositories/__test__/unit/bank-account.prisma.repository.spec.ts
@@ -1,5 +1,82 @@
-describe('Testes de soma', () => {
-  test('1 + 1 é igual a 2', () => {
-    expect(1 + 1).toBe(2)
+import { Test, TestingModule } from '@nestjs/testing'
+
+import { BankAccountEntity } from '@src/modules/bank-accounts/domain/entities/bank-account.entity'
+
+import { BankAccountPrismaRepository } from '@src/modules/bank-accounts/repositories/bank-account.prisma.repository'
+
+import { PrismaService } from '@src/shared/modules/persistence/prisma.service'
+
+describe('BankAccountPrismaRepository', () => {
+  let bankAccountRepository: BankAccountPrismaRepository
+  let prismaService: PrismaService
+
+  let model: any
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        BankAccountPrismaRepository,
+        {
+          provide: PrismaService,
+          useValue: {
+            bankAccount: {
+              create: jest.fn(),
+              deleteMany: jest.fn()
+            }
+          }
+        }
+      ]
+    }).compile()
+
+    bankAccountRepository = module.get<BankAccountPrismaRepository>(
+      BankAccountPrismaRepository
+    )
+    prismaService = module.get<PrismaService>(PrismaService)
+
+    model = prismaService.bankAccount
+  })
+
+  it('should be defined', () => {
+    expect(bankAccountRepository).toBeDefined()
+  })
+
+  describe('save', () => {
+    it('should call prisma method to save bank account on database and return saved bank account entity', async () => {
+      const bankAccount = BankAccountEntity.create({
+        agency: 'Mocked Agency',
+        type: 'CORRENTE',
+        balance: 2000
+      })
+
+      model.create.mockResolvedValue({
+        ...bankAccount,
+        accountNumber: 1,
+        isActive: true
+      })
+
+      const result = await bankAccountRepository.save(bankAccount)
+
+      expect(model.create).toBeCalledTimes(1)
+      expect(model.create).toHaveBeenCalledWith({
+        data: { ...bankAccount }
+      })
+
+      expect(result).toEqual({
+        ...bankAccount,
+        accountNumber: 1,
+        isActive: true
+      })
+    })
+  })
+
+  describe('clear', () => {
+    it('should call prisma method to delete all registers on table', async () => {
+      model.deleteMany.mockResolvedValue()
+
+      await bankAccountRepository.clear()
+
+      expect(model.deleteMany).toBeCalledTimes(1)
+      expect(model.deleteMany).toHaveBeenCalledWith()
+    })
   })
 })
